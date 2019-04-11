bdd800297854b5f40eb65644b90534d663d7f517
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-04-11 20:11:05 +0700

    Update add-product.component.ts

--- src/app/products/add-product/add-product.component.ts
+++ src/app/products/add-product/add-product.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../product/product';
 import { ProductserviceService } from '../service/productservice.service';
 import { CategoryserviceService } from 'src/app/category/services/categoryservice.service';
 import { Category } from 'src/app/category/category';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -23,9 +24,16 @@ export class AddProductComponent implements OnInit {
     this.getCategory();
   }
 
-  addProduct(){   
-    this.productServices.addProduct(this.cateid,this.product).subscribe();
-    this.product = new Product();
+  // addProduct(){   
+  //   this.productServices.addProduct(this.cateid,this.product).subscribe();
+  //   this.product = new Product();
+  // }
+
+  addProduct(formAddProduct: NgModel){
+    if(formAddProduct.valid){
+      this.productServices.addProduct(this.cateid,formAddProduct.value).subscribe();
+    }
+    formAddProduct.reset();
   }
 
   getCategory(){
