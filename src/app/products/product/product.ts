63f7516c52ca7e3348314bcddb45882771f88d5c
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-04-11 11:11:38 +0700

    Create product.ts

---
+++ src/app/products/product/product.ts
@@ -0,0 +1,10 @@
+export class Product{
+    id: number;
+    categoryId: number;
+    name: string;
+    imglink: string;
+    pricesale: number;
+    priceregular: number;
+    review: number;
+    status: boolean = true;
+}
