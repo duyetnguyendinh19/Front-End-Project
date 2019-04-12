214e4862a17bf2aa7738478b6baabf24e5e62e9f
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-04-12 11:38:54 +0700

    Update edit-product.component.ts

--- src/app/products/edit-product/edit-product.component.ts
+++ src/app/products/edit-product/edit-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { DefaultIterableDifferFactory } from '@angular/core/src/change_detection/change_detection';
 import { ProductserviceService } from '../service/productservice.service';
+import { Product } from '../product/product';
+import { CategoryserviceService } from 'src/app/category/services/categoryservice.service';
+import { Category } from 'src/app/category/category';
 
 @Component({
   selector: 'app-edit-product',
@@ -10,14 +12,28 @@ import { ProductserviceService } from '../service/productservice.service';
 export class EditProductComponent implements OnInit {
 
   @Input() dataEdit;
+  @Input() dataShowProduct: Product[];
+  @Input() cateId: number;
+  categorys: Category[];
 
-  constructor(private productServices: ProductserviceService) { }
+  constructor(private productServices: ProductserviceService,private categorySevice: CategoryserviceService) { }
 
   ngOnInit() {
+    this.getCategory();
+  }
+
+  getCategory(){
+    this.categorySevice.getCategory().subscribe(cate => {this.categorys = cate});
   }
 
   saveData(){
-    this.productServices.editProduct(this.dataEdit).subscribe();
-    console.log(this.dataEdit);
+    this.productServices.editProduct(this.dataEdit).subscribe( data =>
+      {
+        if(data.categoryId !== this.cateId){
+          console.log(this.dataShowProduct)
+          console.log(this.dataShowProduct.filter(item => {item.id !== data.id}))
+        }
+      }
+    );
   }
 }
